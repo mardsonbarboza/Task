71725afded986f0945020d2d2d8888a607d6aae5
mardsonbarboza <mardson49@gmail.com>
2024-12-15 11:55:54 -0300

    feat: importação de arquivos e uso do mesmo

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const RouterTask = require('./routers/RouterTask')
+require('dotenv').config();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocs = require('./swaggerConfig'); // Importa a configuração do Swagger
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
-
-
+app.use('/api',RouterTask);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.listen(8080,()=>{
     console.log('server ligado');
 })
